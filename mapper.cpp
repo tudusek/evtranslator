ca33b9dd36a2fc9c38d400486a4450f524fa2bff
tudusek <tadeasjanosek@gmail.com>
2024-04-23 22:34:23 +0200

    Loading configuration from json file

--- mapper.cpp
+++ mapper.cpp
@@ -1,64 +1,18 @@
 #include "mapper.hpp"
 #include <cstdlib>
-#include <cstring>
 #include <iostream>
-#include <linux/input-event-codes.h>
-#include <ostream>
-#include <sstream>
+#include <libevdev-1.0/libevdev/libevdev.h>
 
-evdevMapper::MAP evdevMapper::parseCFG(char *str) {
-  //EV_KEY.KEY_A:EV_KEY.KEY_B
-  //EV_KEY.KEY_A:EV_ABS.ABS_X
-  MAP m;
-  std::stringstream ss(str);
-  ss >> m.in_event_type >> m.in_event_code >> m.out_event_type >>
-      m.out_event_code;
-  return m;
-}
 void evdevMapper::parseParameters(int argc, char *argv[]) {
-  int parserStatus = 0;
-  for (int i = 1; i < argc; i++) {
-    switch (parserStatus) {
-    case PARSE_DEVICE_PATH:
-      in_device_path = argv[i];
-      parserStatus = 0;
-      break;
-    case PARSE_DEVICE_NAME:
-      out_device_name = argv[i];
-      parserStatus = 0;
-      break;
-    case PARSE_DEVICE_CONFIG:
-      config.push_back(parseCFG(argv[i]));
-      parserStatus = 0;
-      break;
-    default:
-      if (parserStatus) {
-        std::cout << "Error while parsing arguments \n";
-        return;
-      }
-      if (!strcmp(argv[i], "-d")) {
-        parserStatus = PARSE_DEVICE_PATH;
-      } else if (!strcmp(argv[i], "-n")) {
-        parserStatus = PARSE_DEVICE_NAME;
-      } else if (!strcmp(argv[i], "-c")) {
-        parserStatus = PARSE_DEVICE_CONFIG;
-      } else if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) {
-        // Help
-        return;
-      }
-      break;
-    }
-  }
-  if (parserStatus) {
-    std::cout << "Error while parsing arguments \n";
-    return;
-  }
-  std::cout << "config" << std::endl;
-  for (int i = 0; i < this->config.size(); i++) {
-    std::cout << this->config[i].in_event_type << " "
-              << this->config[i].in_event_code << " "
-              << this->config[i].out_event_type << " "
-              << this->config[i].out_event_code << std::endl;
+  if (argc == 3) {
+    this->in_device_path = argv[1];
+    std::ifstream f(argv[2]);
+    this->data = json::parse(f);
+  } else {
+    std::cout
+        << "Usage: evdevmapper /path/to/device /path/to/configuration.json"
+        << std::endl;
+    exit(0);
   }
 }
 evdevMapper::evdevMapper(int argc, char *argv[]) {
@@ -87,29 +41,48 @@ void evdevMapper::setupInputDev() {
   }
   std::cout << libevdev_get_name(input_dev) << std::endl;
   // check if the device has all evtypes and evcodes
-  for (int i = 0; i < this->config.size(); i++) {
-    if (!libevdev_has_event_type(input_dev, this->config[i].in_event_type)) {
-      std::cout << "This device doesn't have "
-                << libevdev_event_type_get_name(this->config[i].in_event_type)
-                << std::endl;
-      exit(0);
-    }
-    if (!libevdev_has_event_code(input_dev, this->config[i].in_event_type,
-                                 this->config[i].in_event_code)) {
-      std::cout << "This device doesn't have "
-                << libevdev_event_code_get_name(this->config[i].in_event_type,
-                                                this->config[i].in_event_code)
-                << std::endl;
-      exit(0);
+  if (!this->data["mappings"].is_null()) {
+    for (auto &[evtype, evcodes] : this->data["mappings"].items()) {
+      std::cout << evtype << std::endl;
+      __u16 evt = libevdev_event_type_from_name(evtype.c_str());
+      for (auto &[evcode, evprops] : evcodes.items()) {
+        __u16 evc = libevdev_event_code_from_name(evt, evcode.c_str());
+        if (!libevdev_has_event_code(this->input_dev, evt, evc)) {
+          std::cout << "This device doesn't have " << evtype << "(" << evt
+                    << ")"
+                    << " or " << evcode << "(" << evc << ")" << std::endl;
+          exit(0);
+        }
+      }
     }
   }
 }
 void evdevMapper::setupOutputDev() {
   this->output_dev = libevdev_new();
-  libevdev_set_name(this->output_dev, this->out_device_name.c_str());
-  for (int i = 0; i < this->config.size(); i++) {
-    libevdev_enable_event_code(this->output_dev, this->config[i].out_event_type,
-                               this->config[i].out_event_code, nullptr);
+  if (this->data["device-name"].is_null())
+    libevdev_set_name(this->output_dev, "evdev-mapper");
+  else
+    libevdev_set_name(this->output_dev,
+                      this->data["device-name"].get<std::string>().c_str());
+
+  if (!this->data["mappings"].is_null()) {
+    for (auto &[evtype, evcodes] : this->data["mappings"].items()) {
+      __u16 evt = libevdev_event_type_from_name(evtype.c_str());
+      for (auto &[evcode, evprops] : evcodes.items()) {
+        if (!evprops["event-type"].is_null() &&
+            !evprops["event-code"].is_null()) {
+          __u16 oevt = libevdev_event_type_from_name(
+              evprops["event-type"].get<std::string>().c_str());
+          __u16 oevc = libevdev_event_code_from_name(
+              oevt, evprops["event-code"].get<std::string>().c_str());
+          switch (evt) {
+          case EV_KEY:
+            libevdev_enable_event_code(this->output_dev, oevt, oevc, nullptr);
+            break;
+          }
+        }
+      }
+    }
   }
   libevdev_uinput_create_from_device(
       this->output_dev, LIBEVDEV_UINPUT_OPEN_MANAGED, &this->output_dev_uinput);
@@ -122,28 +95,37 @@ void evdevMapper::eventLoop() {
         this->input_dev,
         LIBEVDEV_READ_FLAG_NORMAL | LIBEVDEV_READ_FLAG_BLOCKING, &ev);
     if (rc == LIBEVDEV_READ_STATUS_SYNC) {
-      // printf("::::::::::::::::::::: dropped ::::::::::::::::::::::\n");
+      printf("::::::::::::::::::::: dropped ::::::::::::::::::::::\n");
       while (rc == LIBEVDEV_READ_STATUS_SYNC) {
         rc = libevdev_next_event(this->input_dev, LIBEVDEV_READ_FLAG_SYNC, &ev);
       }
-      // printf("::::::::::::::::::::: re-synced ::::::::::::::::::::::\n");
+      printf("::::::::::::::::::::: re-synced ::::::::::::::::::::::\n");
     } else if (rc == LIBEVDEV_READ_STATUS_SUCCESS) {
       // handle events
       std::cout << "Event:" << std::endl
                 << "  " << ev.type << " " << ev.code << " " << ev.value
                 << std::endl;
-      for (int i = 0; i < this->config.size(); i++) {
-        if (ev.type != EV_SYN && ev.type == config[i].in_event_type &&
-            ev.code == config[i].in_event_code) {
-          std::cout << "  Found cfg:" << std::endl
-                    << "    " << ev.type << " " << ev.code << " "
-                    << config[i].out_event_type << " "
-                    << config[i].out_event_code << std::endl;
-          libevdev_uinput_write_event(this->output_dev_uinput,
-                                      this->config[i].out_event_type,
-                                      this->config[i].out_event_code, ev.value);
-          libevdev_uinput_write_event(this->output_dev_uinput, EV_SYN,
-                                      SYN_REPORT, 0);
+
+      for (auto &[evtype, evcodes] : this->data["mappings"].items()) {
+        __u16 evt = libevdev_event_type_from_name(evtype.c_str());
+        for (auto &[evcode, evprops] : evcodes.items()) {
+          __u16 evc = libevdev_event_code_from_name(evt, evcode.c_str());
+          if (ev.type != EV_SYN && ev.type == evt && ev.code == evc) {
+            if (!evprops["event-type"].is_null() &&
+                !evprops["event-code"].is_null()) {
+              __u16 oevt = libevdev_event_type_from_name(
+                  evprops["event-type"].get<std::string>().c_str());
+              __u16 oevc = libevdev_event_code_from_name(
+                  oevt, evprops["event-code"].get<std::string>().c_str());
+              std::cout << "  Found cfg:" << std::endl
+                        << "    " << ev.type << " " << ev.code << " " << oevt
+                        << " " << oevc << std::endl;
+              libevdev_uinput_write_event(this->output_dev_uinput, oevt, oevc,
+                                          ev.value);
+              libevdev_uinput_write_event(this->output_dev_uinput, EV_SYN,
+                                          SYN_REPORT, 0);
+            }
+          }
         }
       }
     }
--- mapper.hpp
+++ mapper.hpp
@@ -1,5 +1,8 @@
+#include <cstdlib>
+#include <iostream>
+#include <ostream>
+#include <fstream>
 #include <string>
-#include <vector>
 #include <fcntl.h>
 #include <linux/input.h>
 #include <linux/uinput.h>
@@ -8,30 +11,22 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <nlohmann/json.hpp>
+#include <linux/input-event-codes.h>
+#include <nlohmann/json_fwd.hpp>
+
+using json = nlohmann::json;
 
 class evdevMapper {
 public:
-  enum { 
-    PARSE_DEVICE_PATH = 1,
-    PARSE_DEVICE_NAME,
-    PARSE_DEVICE_CONFIG
-  };
-  typedef struct {
-    __u16 in_event_type;
-    __u16 in_event_code;
-    __u16 out_event_type;
-    __u16 out_event_code;
-  } MAP;
   evdevMapper(int argc, char* argv[]); 
   ~evdevMapper();
 private:
-  std::vector<MAP> config;
   std::string in_device_path = "";
-  std::string out_device_name = "";
+  json data;
   struct libevdev *input_dev = nullptr;
   struct libevdev *output_dev = nullptr;
   struct libevdev_uinput *output_dev_uinput = nullptr;
-  MAP parseCFG(char *str);
   void parseParameters(int argc, char* argv[]);
   void setupInputDev();
   void setupOutputDev();
